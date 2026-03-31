e978bdfd5f9047e6fa2bb054a53fda3dc7a9e617
kominjung04 <chunguharu@gmail.com>
2026-03-31 15:26:22 +0900

    feat: VariableDeclare

---
+++ src/p01Variable/J01VariableDeclare.java
@@ -0,0 +1,59 @@
+package p01Variable;
+
+/*
+* 멤버변수와 메서드는 클래스 없이는 선언될 수 없다.
+* 클래스는 생성연산자를 new를 만나서 인스턴스를 생성해야 사용가능.
+* 프로그램의 시작은 main()에서 시작한다.모든 클래스에 선언 가능.
+*/
+
+// file을 대표하는 클래스에는 반드시 public을 붙이고, 파일명과 클래스명 동일
+public class J01VariableDeclare {
+  //변수의 선언, 값이 없으면 기본값으로 초기화
+  int num; // 0
+
+  //메서드 선언
+  void add() {
+  }
+
+  //  main()은 자바 프로그램 실행하기 위한 Entry Point
+  //  main()이 들어 있는 클래스는 실행 가능
+  //  main()의 실행은 JVM이 한다.
+  //  main() 안에 내용들이 순차적으로 진행됨
+  public static void main(String[] args) {
+    System.out.println("hello");
+    //선언                  =  주소값으로 초기화 :: 할당
+    J01VariableDeclare j01 = new J01VariableDeclare(); //instance
+    System.out.println(j01.num);
+    j01.add();
+    J01VariableDeclare j02 = new J01VariableDeclare();
+    j02.num = 100;
+    System.out.println(j02.num);
+    Student s1 = new Student();
+    Apple a1 = new Apple();
+    s1.eatApple(a1);
+
+    //선언 = 초기화 :: 할당
+    //int a = 10;
+  }
+}
+
+class Student {
+  void eatApple(Apple apple){
+    System.out.println(apple + "먹다");
+  }
+}
+
+class Apple {
+  @Override
+  public String toString() {
+    return "사과";
+  }
+}
+
+// 단축키
+// shift + enter : 자리에서 엔터
+//ctrl + shift + enter:자동 중괄호 생성
+//ctrl + alt + L :자동 정렬
+//alt + shift + f10 : run
+//ctrl + / : 주석
+//ctrl + d : 줄복사
