8121343eaace1b2f6ca2246df253d1720cffbcf3
kominjung04 <chunguharu@gmail.com>
2026-04-13 17:37:28 +0900

    FEAT:Modifier(접근 제어자)

---
+++ src/p04Class/J16Modifier.java
@@ -0,0 +1,12 @@
+package p04Class;
+
+public class J16Modifier {
+  public static void main(String[] args) {
+    //  * [ 제어자(Modifier) ]
+    //  * class  :: public,(default),final,abstract
+    //  * method :: All(public,protected,default,private),final,abstract,static
+    //  * 멤버 변수 :: All, final, static
+    //  * 지역 변수 :: final
+
+  }
+}
