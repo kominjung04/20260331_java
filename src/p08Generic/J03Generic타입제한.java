08698ec96868992358ff02ee1e58a9ee188907da
kominjung04 <chunguharu@gmail.com>
2026-04-20 17:21:26 +0900

    FEAT: Generic 타입 제한

--- src/p08Generic/J03Generic타입제한.java
+++ src/p08Generic/J03Generic타입제한.java
@@ -5,4 +5,21 @@ public class J03Generic타입제한 {
 
   }
 }
+class  Gen1{
+  String name = "Gen1";
+  public String getName(){return name;}
+  public void setName(String name){this.name =name;}
 
+  class Gen2 extends Gen1 {
+    public Gen2() {
+      setName("Gen2");
+    }
+  }
+
+  class Gen3 extends Gen2{
+    public Gen3(){
+      setName("Gen3");
+    }
+  }
+
+}
