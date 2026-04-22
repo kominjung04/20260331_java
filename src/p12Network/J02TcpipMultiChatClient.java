2364f9e56e18242acfba6dd6ec0ac535d745e00e
kominjung04 <chunguharu@gmail.com>
2026-04-22 17:41:29 +0900

    FEAT: TCP/IP MultiChat Client

---
+++ src/p12Network/J02TcpipMultiChatClient.java
@@ -0,0 +1,16 @@
+package p12Network;
+
+import javax.swing.*;
+import java.awt.*;
+
+public class J02TcpipMultiChatClient {
+  public static void main(String[] args) {
+
+  }
+}
+
+class MyChat extends JFrame{
+  public MyChat() throws HeadlessException {
+
+  }
+}
